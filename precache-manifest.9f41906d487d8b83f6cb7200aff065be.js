7ef8dc147fb6b0aee8cfa5b924f1b00f7d21d5df
Yosef <jossyfreelancer@gmail.com>
2024-11-29 14:21:29 +0300

    Updates

---
+++ precache-manifest.9f41906d487d8b83f6cb7200aff065be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82b8761743c83929c3a3be8966ccfdd7",
+    "url": "/Netflix-Clone/index.html"
+  },
+  {
+    "revision": "a7af52f375d56f132d76",
+    "url": "/Netflix-Clone/static/css/main.bcd14408.chunk.css"
+  },
+  {
+    "revision": "305596199e88fd13b192",
+    "url": "/Netflix-Clone/static/js/2.cd713ae0.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/Netflix-Clone/static/js/2.cd713ae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7af52f375d56f132d76",
+    "url": "/Netflix-Clone/static/js/main.56d77e5d.chunk.js"
+  },
+  {
+    "revision": "b2b8b24758047419b104",
+    "url": "/Netflix-Clone/static/js/runtime-main.f28e37b4.js"
+  }
+]);
